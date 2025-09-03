7ef7b56c49d824017454dcd8c769831e5442439f
Rebyuu <86248240+Rebyuu@users.noreply.github.com>
2025-09-03 15:28:59 +0200

    logging server adjusted

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const SAVE_PATH = './canvas_data.json';
 const wss = new WebSocket.Server({ port: PORT });
 
-console.log('✅ WebSocket-Server läuft auf port ${PORT}');
+console.log(`Server running on port ${PORT}`);
 
 let clientCounter = 0;
 
