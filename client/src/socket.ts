0f29cd51dd9a8f28e8ae960036a75842a5b8fa2f
Rebyuu <86248240+Rebyuu@users.noreply.github.com>
2025-09-08 10:19:38 +0200

    client adjustments to be render ready (port removed)

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,7 +3,7 @@ let socket: WebSocket;
 
 export const getSocket = () => {
   if (!socket || socket.readyState === WebSocket.CLOSED) {
-    socket = new WebSocket('wss://draw4friends-backend.onrender.com:10000');
+    socket = new WebSocket('wss://draw4friends-backend.onrender.com');
   }
   return socket;
 };
